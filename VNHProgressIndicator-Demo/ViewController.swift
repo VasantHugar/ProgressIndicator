817612867c096ff62952cf6023483fd0cdc18c29
Vasant Hugar <vasant.hugar@indpro.se>
2018-06-26 19:23:42 +0530

    Initial Commit

---
+++ VNHProgressIndicator-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VNHProgressIndicator-Demo
+//
+//  Created by Vasant Hugar on 26/06/18.
+//  Copyright © 2018 Vasant Hugar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
